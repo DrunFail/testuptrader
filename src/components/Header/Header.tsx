cfc7a110fa076614a3e3d4d54e27a26f104cfd53
Andrew <andrew.novikov@outlook.com>
2022-11-29 10:42:36 +0300

    реализована логика поиска по id и title

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,18 @@
 import { Link } from 'react-router-dom';
+import Search from '../Search/Search';
 import styles from './Header.module.scss';
 
 
 export default function Header() {
     return (
         <header className={styles.header}>
+            <Search />
+
             <nav className={styles.navbar}>
                 <ul>
                    <li><Link to='/'>projects</Link></li>                
-                   <li><Link to='todo'>todo</Link></li>
                 </ul>
             </nav>
-
         </header>
     );
 }
---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { FAKE_DATA } from "../../data/data";
+import { Todo } from "../../interfaces/interfaces";
+
+export default function Search() {
+    const [dataSearch, setDataSearch] = useState('')
+
+    let searched: Todo[] = []
+
+    const search = (array: Todo[]) => {
+        if (typeof +dataSearch == 'number') {
+            array.map(elem => {
+                if (elem.id == +dataSearch) {
+                    searched.push(elem)
+                }
+                if (elem.nestedTodo.length) {
+                    return search(elem.nestedTodo)
+                }
+            })
+        }
+
+        if (typeof dataSearch == 'string') {
+            console.log('here')
+            array.map(elem => {
+                if (elem.title.toLowerCase().includes(dataSearch.toLowerCase())) {
+                    searched.push(elem)
+                }
+                if (elem.nestedTodo.length) {
+                    return search(elem.nestedTodo)
+                }
+            })
+        }
+    }
+    search(FAKE_DATA)
+    
+    return (
+        <form>
+            <label>search</label>
+            <input
+                value={dataSearch}
+                type='search'
+                onChange={(e) => setDataSearch(e.target.value) }
+            />
+        </form>
+        );
+}
