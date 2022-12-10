38b0a3e40b1a9f3506f7100ac184d477a6dde123
Andrew <andrew.novikov@outlook.com>
2022-12-10 23:21:24 +0300

    fix redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,8 +3,11 @@ import rootReducer from './rootReducer';
 import thunk from "redux-thunk"
 
 
+const composeSetup = process.env.NODE_ENV !== 'production' && typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
 
-const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(rootReducer, composeSetup(applyMiddleware(thunk)));
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
